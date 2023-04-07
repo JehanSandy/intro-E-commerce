dc2371ef99a8a9ef6f8d00e4e58ad1177e38b085
nafis <nafiezlela9@gmail.com>
2023-04-07 10:38:20 +0700

    conflic

--- src/collabolator/book.jsx
+++ src/collabolator/book.jsx
@@ -63,9 +63,6 @@ class Navibook extends React.Component {
 }
 
 const styles = {
-  navibar: {
-    // backgroundColor: "rgba(0, 25, 112, 0.7)",
-  },
   btn: {
     border: "none",
     backgroundColor: "#5343bae",
